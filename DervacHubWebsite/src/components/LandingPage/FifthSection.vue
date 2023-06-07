6d2c0f447763201e3e6ea11d76e76e052282722e
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-07 14:47:02 +0100

    init commit

---
+++ DervacHubWebsite/src/components/LandingPage/FifthSection.vue
@@ -0,0 +1,17 @@
+<script>
+   import { defineComponent } from 'vue'
+
+   export default defineComponent({
+    data(){
+        return{
+
+        }
+    }
+   })
+</script>
+
+<template>
+    <section id="fifth-section">
+
+    </section>
+</template>
