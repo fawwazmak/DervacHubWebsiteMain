a79f90a62d56760c2fd495f086b7c1204855a1a2
fawwazmak <fawwazmakinde47@gmail.com>
2023-06-08 12:40:00 +0100

    first section and navbar with footer responsiveness done

--- DervacHubWebsite/src/components/Footer.vue
+++ DervacHubWebsite/src/components/Footer.vue
@@ -19,7 +19,7 @@ text-[#224c75] ">
             <div class="flex flex-col space-y-2">
 
                 <!-- logo -->
-                <div class="flex">
+                <div class="flex lg:w-[15%] w-[40%]">
                     <img src="../assets/images/dervac-main-logo.svg" alt="">
                     <img src="../assets/images/logo-complement.svg" alt="">
                 </div>
@@ -36,8 +36,8 @@ text-[#224c75] ">
             <p>&copy 2023 dervac.co</p>
         </div>
 
-        <ul class="flex flex-col space-y-2">
-            <li class="text-[18px] font-bold">Company</li>
+        <ul class="flex flex-col space-y-2 md:text-left text-center">
+            <li class="md:text-[18px] text-[22px] font-bold">Company</li>
             <li>Fellowship</li>
             <li>Training</li>
             <li>Testiminials</li>
@@ -45,23 +45,23 @@ text-[#224c75] ">
             <li>FAQS</li>
         </ul>
         
-        <ul class="flex flex-col space-y-2">
-            <li class="text-[18px] font-bold">Courses</li>
+        <ul class="flex flex-col space-y-2 md:text-left text-center">
+            <li class="md:text-[18px] text-[22px] font-bold">Courses</li>
             <li>Web Dev.</li>
-            <li>Mobile Alili</li>
+            <li>Mobile App</li>
             <li>Design</li>
             <li>Engineering</li>
         </ul>
 
-        <ul class="flex flex-col space-y-2">
-            <li class="text-[18px] font-bold">Resources</li>
+        <ul class="flex flex-col space-y-2 md:text-left text-center">
+            <li class="md:text-[18px] text-[22px] font-bold">Resources</li>
             <li>About us</li>
             <li>Contact</li>
             <li>Admissios</li>
         </ul>
 
-        <ul class="flex flex-col space-y-2">
-            <li class="text-[18px] font-bold">More</li>
+        <ul class="flex flex-col space-y-2 md:text-left text-center">
+            <li class="md:text-[18px] text-[22px] font-bold">More</li>
             <li>Terms</li>
             <li>Privacy</li>
             <li>Help</li>
@@ -70,12 +70,12 @@ text-[#224c75] ">
 
         <!-- this is for the logo, social-media handle and the copywright for other view-->
         <div class="md:hidden flex flex-col items-center space-y-4">
-            <div class="flex flex-col space-y-2">
+            <div class="flex flex-col space-y-2 items-center">
 
                 <!-- logo -->
-                <div class="flex">
-                    <img src="../assets/images/dervac-main-logo.svg" alt="">
-                    <img src="../assets/images/logo-complement.svg" alt="">
+                <div class="flex w-[80%] mx-auto items-center">
+                    <img src="../assets/images/dervac-main-logo.svg" alt="" class="w-[23px]">
+                    <img src="../assets/images/logo-complement.svg" alt="" class="w-[110px]">
                 </div>
 
                 <!-- social-media-links -->
