a7c64aa93ab8e83858ffde38dc4a70d3d4190c1a
fawwazmak <fawwazmakinde47@gmail.com>
2023-06-09 10:17:19 +0100

    the tenth section has been completed

--- DervacHubWebsite/src/components/LandingPage/TenthSection.vue
+++ DervacHubWebsite/src/components/LandingPage/TenthSection.vue
@@ -12,12 +12,12 @@ export default defineComponent({
 
 
 <template>
-    <div class="bg-[#11263b] w-[90%] mx-auto text-white text-center py-16 rounded-[10px] my-20">
-        <p class="text-[40px] font-bold">Start your career</p>
+    <div class="bg-[#11263b] w-[85%] mx-auto mb-6 text-white text-center md:py-16 py-8 px-6 rounded-[10px] my-20 items-center flex flex-col gap-6">
+        <p class="md:text-[40px] text-[23px] font-bold">Start your career</p>
         <p class="">Join over 4,000+ students already growing with Dervac</p>
-        <div>
-            <component is="Button" class="bg-white tetx-[#11263b]" value="Learn more"></component>
-            <component is="Button" class="bg-[#11263b]" value="Join now"></component>
+        <div class="flex gap-4 md:flex-row flex-col md:w-fit w-full">
+            <component is="Button" class="bg-white text-[#11263b] py-2 px-4 rounded-[5px]" value="Learn more">Learn more</component>
+            <component is="Button" class="bg-[#224C75] py-2 px-4 rounded-[5px]" value="Join now">Join now</component>
         </div>
     </div>
 </template>
