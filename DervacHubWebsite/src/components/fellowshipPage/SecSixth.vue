45d5b392cf021bfab8542096992c472dcc1c6074
AbdulHameedi <sanilinity507@gmail.com>
2023-06-10 00:00:55 +0100

    admission

---
+++ DervacHubWebsite/src/components/fellowshipPage/SecSixth.vue
@@ -0,0 +1,14 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: []
+})
+</script>
+<template>
+            <div class="flex flex-col items-center">
+                <p class="text-[24px] mb-4 p-3 font-bold px-6 w-fit rounded-[50%] text-center bg-[#E9EEF3] text-[#000000]">1</p>
+                <p class="text-[20px] mb-3 text-[#101828] font-bold">Fill and submit the application form</p>
+                <p class="text-[16px] text-center text-[#475467]">Lorem ipsum dolor sit amet consectetur. Facilisis venenatis nascetur volutpat quis diam tristique metus nulla hendrerit.</p>
+            </div>
+</template>
---
+++ DervacHubWebsite/src/components/fellowshipPage/Sixth.vue
@@ -0,0 +1,22 @@
+<script>
+import { defineComponent } from 'vue';
+import SecSixth from './SecSixth.vue';
+
+export default defineComponent({
+    components: { SecSixth}
+
+})
+</script>
+<template>
+    <div class="py-[96px] bg-white px-20">
+        <div class="flex justify-center flex-col items-center mb-[4.5rem]">
+            <p class="text-[36px] text-[#224C75] font-semibold mb-4">How the Admission Process Works</p>
+            <p class="text-[20px] text-[#475467] max-w-[770px] text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, nisi quod animi commodi dolore nesciunt deleniti. Aut.</p>
+        </div>
+        <div class="flex justify-between items-center">
+            <SecSixth heading="Fill and submit the application form"/>
+            <SecSixth/>
+            <SecSixth/>
+        </div>
+    </div>
+</template>
