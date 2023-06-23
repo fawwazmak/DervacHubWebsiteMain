df3e8e618ec68404a0fb83e1de4cc307be67b822
fawwazmak <fawwazmakinde47@gmail.com>
2023-06-23 12:43:39 +0100

    changes in progress

---
+++ DervacHubWebsite/src/components/AboutUsPage/History&teamPage/ThirdSection.vue
@@ -0,0 +1,19 @@
+<script>
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+    
+})
+</script>
+
+
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+<style></style>
