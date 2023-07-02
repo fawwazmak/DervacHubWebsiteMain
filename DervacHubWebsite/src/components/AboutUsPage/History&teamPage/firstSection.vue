fba740c02bbeb75dc45047f429f551b84a84c850
fawwazmak <fawwazmakinde47@gmail.com>
2023-07-02 11:30:19 +0100

    adjusted  some mistake

--- DervacHubWebsite/src/components/AboutUsPage/History&teamPage/firstSection.vue
+++ DervacHubWebsite/src/components/AboutUsPage/History&teamPage/firstSection.vue
@@ -1,9 +1,9 @@
 <script></script>
 <template>
-    <div class="w-full flex flex-col gap-[1rem] items-center justify-between p-[1rem] pt-[4rem] text-center bg-gradient-to-br from-blue-100 via-white via-white via-white to-pink-200">
-        <h1 class="text-[40px] font-[700]">About</h1>
+    <div class="w-full flex flex-col gap-[1rem] items-center justify-between pb-20 pt-36 text-center bg-gradient-to-br from-blue-100 via-white via-white via-white to-pink-200">
+        <h1 class="text-[40px] font-[700]">About Us</h1>
         <p>Hear form some of our amazing Students</p>
-        <div class="flex w-[100%] gap-[1rem] lg:flex-row flex-col">
+        <!-- <div class="flex w-[100%] gap-2 px-4 lg:flex-row flex-col mt-20">
             <div class="lg:w-[48%] w-full flex md:flex-row flex-col gap-[1rem]">
                 <div class="md:w-[55%] lg:h-[65vh] md:h-55vh h-[50vh] w-full">
                     <img class="h-full w-full" src="../images/Frame 39639.png" alt="">
@@ -20,6 +20,10 @@
                     <img class="h-full w-full" src="../images/Frame 39640.png" alt="">
                 </div>
             </div>
+        </div> -->
+
+        <div>
+            
         </div>
     </div>
 </template>
